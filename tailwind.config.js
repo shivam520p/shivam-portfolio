b4b3ee3ab7d4d4de1d6085f9e27212a85964b1f5
shivam520p <146962331+shivam520p@users.noreply.github.com>
2024-12-06 23:36:45 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add paths for all files using Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
